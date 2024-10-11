e74bec1474b675f68f65f121c82f8cec092ccac5
nblt <ltwtzjh@126.com>
2024-10-11 12:07:27 +0800

    update grad accumulate

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ lmbda=0.95
 opt=FriendlySAM # FriendlySAM SAM
 DST=results/$opt/$datasets/$model/$opt\_cutout\_$rho\_$sigma\_$lmbda\_$epoch\_$model\_bz$bz\_wd$wd\_$datasets\_$schedule\_seed$seed
 
-CUDA_VISIBLE_DEVICES=$device python -u train.py --datasets $datasets \
+CUDA_VISIBLE_DEVICES=$device python -u train_gradacc.py --datasets $datasets \
         --arch=$model --epochs=$epoch --wd=$wd --randomseed $seed --lr 0.05 --rho $rho --optimizer $opt \
         --save-dir=$DST/checkpoints --log-dir=$DST -p 200 --schedule $schedule -b $bz \
-        --cutout --sigma $sigma --lmbda $lmbda
+        --cutout --sigma $sigma --lmbda $lmbda --grad-acc 3
